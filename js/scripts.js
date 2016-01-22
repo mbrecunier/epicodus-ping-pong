cf8087e0a0cd8a53a8c8a2fae41f0174251f44c0
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 09:04:11 -0800

    make pingPong replace numbers divisible by 3 and 5 with pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ var pingPong = function(userNumber) {
     numberArray.push(index);
   }
   numberArray.forEach(function(number) {
-    if (number % 3 === 0) {
+    if((number % 5 === 0) && (number % 3 === 0)) {
+      numberArray[number-1] = 'pingpong';
+    } else if (number % 3 === 0) {
       numberArray[number-1] = 'ping';
     } else if (number % 5 === 0) {
       numberArray[number-1] = 'pong';
