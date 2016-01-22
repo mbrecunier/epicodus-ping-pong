38b97e603bc51aad8b115b9534259ac05ffd5a1a
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 09:47:38 -0800

    add functionality for negative userNumbers being passed through pingPong

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,32 @@
 var pingPong = function(userNumber) {
   var numberArray = [];
-  for (var index = 1; index <= userNumber; index++) {
-    numberArray.push(index);
-  }
-  numberArray.forEach(function(number) {
-    if((number % 5 === 0) && (number % 3 === 0)) {
-      numberArray[number-1] = 'pingpong';
-    } else if (number % 3 === 0) {
-      numberArray[number-1] = 'ping';
-    } else if (number % 5 === 0) {
-      numberArray[number-1] = 'pong';
+  if (userNumber < 0) {
+    for (var index = -1; index >= userNumber; index--) {
+      numberArray.push(index);
     }
-  });
+    numberArray.forEach(function(number) {
+      if((number % 5 === 0) && (number % 3 === 0)) {
+        numberArray[-number-1] = 'pingpong';
+      } else if (number % 3 === 0) {
+        numberArray[-number-1] = 'ping';
+      } else if (number % 5 === 0) {
+        numberArray[-number-1] = 'pong';
+      }
+    });
+  } else if (userNumber > 0) {
+      for (var index = 1; index <= userNumber; index++) {
+        numberArray.push(index);
+      }
+      numberArray.forEach(function(number) {
+        if((number % 5 === 0) && (number % 3 === 0)) {
+          numberArray[number-1] = 'pingpong';
+        } else if (number % 3 === 0) {
+          numberArray[number-1] = 'ping';
+        } else if (number % 5 === 0) {
+          numberArray[number-1] = 'pong';
+        }
+      });
+  }
+
   return numberArray;
 }
