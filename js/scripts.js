5b10499b7798136f0d40a6497a586383ed18e0a8
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 08:49:17 -0800

    make pingPong replace numbers divisible by 3 with ping by looping through the array

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,10 @@ var pingPong = function(userNumber) {
   for (var index = 1; index <= userNumber; index++) {
     numberArray.push(index);
   }
+  numberArray.forEach(function(number) {
+    if (number % 3 === 0) {
+      numberArray[number-1] = 'ping';
+    }
+  });
   return numberArray;
 }
