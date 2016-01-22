2dd13850f45b95b6934733b7372db97ed07684de
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 08:31:24 -0800

    define pingPong as a function to return a numberArray from 1 to userNumber

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var pingPong = function(userNumber) {
+  var numberArray = [];
+  for (var index = 1; index <= userNumber; index++) {
+    numberArray.push(index);
+  }
+  return numberArray;
+}
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,5 @@
 describe('pingPong', function() {
   it('returns an array of numbers counting by 1 from 1 to the chosen number', function() {
-    expect(pingPong(10)).to.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    expect(pingPong(10)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
   });
 });
