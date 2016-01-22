467dc7a709dbc8965dd814a4fb263d08ea09437a
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 08:25:04 -0800

    write spec for returning an array from 1 to userNumber

---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('pingPong', function() {
+  it('returns an array of numbers counting by 1 from 1 to the chosen number', function() {
+    expect(pingPong(10)).to.equal([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+});
