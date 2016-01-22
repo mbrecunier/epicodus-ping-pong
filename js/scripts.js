a33d4a838e6bf9f83b6d8f43f15a3ab56ba8e753
Michelle Brecunier <michelle.brecunier@gmail.com>
2016-01-22 10:39:33 -0800

    add displayResultList function and submit listener for running pingPong

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 var pingPong = function(userNumber) {
   var numberArray = [];
-  if (userNumber < 0) {
+  if (userNumber < 0) { //negative numbers
     for (var index = -1; index >= userNumber; index--) {
       numberArray.push(index);
     }
@@ -13,7 +13,7 @@ var pingPong = function(userNumber) {
         numberArray[-number-1] = 'pong';
       }
     });
-  } else if (userNumber > 0) {
+  } else if (userNumber > 0) { //positive numbers
       for (var index = 1; index <= userNumber; index++) {
         numberArray.push(index);
       }
@@ -27,6 +27,23 @@ var pingPong = function(userNumber) {
         }
       });
   }
-
   return numberArray;
 }
+
+var displayResultList = function(outputArray) {
+    $('ul#result').empty();
+  outputArray.forEach(function(item) {
+    $('ul#result').append('<li>' + item + '</li>');
+  });
+  return;
+}
+
+$(document).ready(function() {
+  $('form#pingPong').submit(function(event) {
+    var userNumber = $('input#userNumber').val();
+
+    displayResultList(pingPong(userNumber));
+
+    event.preventDefault();
+  });
+});
